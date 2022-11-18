9f43320d7df7564b1637807f21d14776f5a2f666
Sebastian Kim <sydneyitguy@gmail.com>
2022-11-18 16:59:07 +0900

    Use test name

--- contracts/Building.sol
+++ contracts/Building.sol
@@ -17,7 +17,7 @@ contract Building is ERC721, ERC721Enumerable, Ownable {
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("HUNT Building", "BUILDING") {}
+    constructor() ERC721("HT Building TEST", "BUILDING") {}
 
     function safeMint(address to) external onlyOwner returns(uint256 tokenId) {
         tokenId = _tokenIdCounter.current();
