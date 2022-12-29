89f177c77f3b921bf1f0b3a7a17fbfced94c130f
Sebastian Kim <sydneyitguy@gmail.com>
2022-12-29 16:53:31 +0900

    Add public RPCs for external contributors

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,13 @@ require('dotenv').config();
 
 require("@nomicfoundation/hardhat-toolbox");
 
+const ETH_MAINNET_RPC = process.env.ALCHEMY_ETH_API_KEY ?
+  `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_ETH_API_KEY}` :
+  "https://eth.llamarpc.com";
+const ETH_GOERLI_RPC = process.env.ALCHEMY_GOERLI_API_KEY ?
+  `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_ETH_API_KEY}` :
+  "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
@@ -16,12 +23,12 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_ETH_API_KEY}`,
+        url: ETH_MAINNET_RPC,
         blockNumber: 16288578
       }
     },
     goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_GOERLI_API_KEY}`,
+      url: ETH_GOERLI_RPC,
       chainId: 5,
       accounts: [process.env.GOERLI_TEST_PRIVATE_KEY]
     },
@@ -32,7 +39,7 @@ module.exports = {
       accounts: [process.env.GOERLI_TEST_PRIVATE_KEY]
     },
     ethmain: {
-      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_ETH_API_KEY}`,
+      url: ETH_MAINNET_RPC,
       chainId: 1,
       accounts: [process.env.ETH_PRIVATE_KEY]
     }
