cc687c10df33a17fc3ae6f0b56495ea1c2d63b57
Sebastian Kim <sydneyitguy@gmail.com>
2025-11-07 19:39:43 +0900

    Update ignition data

--- MintPad/ignition/modules/Mintpad.ts
+++ MintPad/ignition/modules/Mintpad.ts
@@ -3,9 +3,10 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 export default buildModule("Mintpad", (m) => {
   // Use deployer address as default signer if no parameter provided
   const signerAddress = m.getParameter("signerAddress", m.getAccount(0));
-  const bondAddress = m.getParameter("bondAddress", "0xc5a076cad94176c2996B32d8466Be1cE757FAa27");
+  // dailyHuntReward in Wei (TEST: 10 HUNT per day)
+  const dailyHuntReward = m.getParameter("dailyHuntReward", 10n * 10n ** 18n);
 
-  const mintpad = m.contract("Mintpad", [signerAddress, bondAddress]);
+  const mintpad = m.contract("Mintpad", [signerAddress, dailyHuntReward]);
 
   return { mintpad };
 });
